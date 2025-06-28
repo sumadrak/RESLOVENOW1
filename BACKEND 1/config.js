4fe3e098a5c65af3f81a6e3dfad7673d5dc4a9e7
sumadrak <166539548+sumadrak@users.noreply.github.com>
2025-06-28 22:41:35 +0530

    Update config.js

--- BACKEND 1/config.js
+++ BACKEND 1/config.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-mongoose.connect("mongodb://localhost:27017")
+mongoose.connect("mongodb+srv://sumanthdraksharapu8:Sumanth%40123@cluster0.jbq6zgr.mongodb.net/")
 .then(()=>{
    console.log("connected to mongodb")
 })
